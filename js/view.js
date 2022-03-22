fe61e2c5771bbf1756f84d60153ffb7772186dbc
Wilbert Fangderson <wilbertf38@gmail.com>
2022-03-23 05:09:06 +0700

    Zoom created

--- js/view.js
+++ js/view.js
@@ -1,53 +1,69 @@
 /* fungsi-fungsi untuk handle camera angle, zoom, dan view */
 // function cameraAngle() {
-//     var pi = Math.PI;
-//     let value = document.getElementById('cameraAngle').value;
-//     value = degToRad(value);
-
-//     let move = value - oldValueMove;
-
-//     matrix = multiply(yRotation(move), matrix);
-//     for(var i = 0; i<objects.length; i++){
-//         draw(objects[i].projMatrix, objects[i].modelMatrix, objects[i].offset, objects[i].end);  
-//     }
-
-//     oldValueMove = value;
+//     var sliderZ = document.getElementById("zscale");
+//     var valZ = document.getElementById("z-val-scale");
+//     valZ.innerHTML = sliderZ.value/100;
 // }
 
-// function zoomIn() {
-//     matrix = multiply(scale(1.1, 1.1, 1.1), matrix);
-//     for(var i = 0; i<objects.length; i++){
-//         draw(objects[i].projMatrix, objects[i].modelMatrix, objects[i].offset, objects[i].end);  
-//     }
-// }
-
-// function zoomOut() {
-//     view_matrix = multiply(scale(0.9, 0.9, 0.9), view_matrix);
-//     for(var i = 0; i<objects.length; i++){
-//         draw(objects[i].projMatrix, objects[i].modelMatrix, objects[i].offset, objects[i].end);  
-//     }
-// }
-
-function resetView(){
-    console.log(states.objects);
+function zoomOut() {
+    var sliderX = document.getElementById("xscale");
+    var valX = document.getElementById("x-val-scale");
+    var sliderY = document.getElementById("yscale");
+    var valY = document.getElementById("y-val-scale");
+    var sliderZ = document.getElementById("zscale");
+    var valZ = document.getElementById("z-val-scale");
+    valX.innerHTML = sliderX.value/100;
+    valY.innerHTML = sliderY.value/100;
+    valZ.innerHTML = sliderZ.value/100;
     for (let i=0; i < states.objects.length; i++) {
         console.log(states.objects[i].name);
+        sliderX.value *= 1.1
+        sliderY.value *= 1.1
+        sliderZ.value *= 1.1
+        states.objects[i].xScale(sliderX.value/100);
+        states.objects[i].yScale(sliderX.value/100);
+        states.objects[i].zScale(sliderX.value/100);
+    }
+    states.drawAll();
+}
+
+function zoomIn() {
+    var sliderX = document.getElementById("xscale");
+    var valX = document.getElementById("x-val-scale");
+    var sliderY = document.getElementById("yscale");
+    var valY = document.getElementById("y-val-scale");
+    var sliderZ = document.getElementById("zscale");
+    var valZ = document.getElementById("z-val-scale");
+    valX.innerHTML = sliderX.value/100;
+    valY.innerHTML = sliderY.value/100;
+    valZ.innerHTML = sliderZ.value/100;
+    for (let i=0; i < states.objects.length; i++) {
+        console.log(states.objects[i].name);
+        sliderX.value *= 0.9
+        sliderY.value *= 0.9
+        sliderZ.value *= 0.9
+        states.objects[i].xScale(sliderX.value/100);
+        states.objects[i].yScale(sliderX.value/100);
+        states.objects[i].zScale(sliderX.value/100);
+    }
+    states.drawAll();
+}
+
+function resetView(){
+    for (let i=0; i < states.objects.length; i++) {
         if (states.objects[i].name == "CUBE") {
-            console.log("cube");
             states.objects[i].position = states.objects[i].backupArrPosition;
             states.objects[i].center = [400, 80, 80];
             states.objects[i].rotate = [30, 0, 0];
             states.objects[i].translation = [150, 200, 100];
             states.objects[i].scale = [1,1,1];
         } else if (states.objects[i].name == "CUBEOCTAHEDRON") {
-            console.log("cubehedron");
             states.objects[i].position = states.objects[i].backupArrPosition;
             states.objects[i].center = [80, 60, 0];
             states.objects[i].rotate = [90, 0, 0];
             states.objects[i].translation = [300, 300, 100];
             states.objects[i].scale = [1,1,1];   
         } else if (states.objects[i].name == "PYRAMID") {
-            console.log("piramid");
             states.objects[i].position = states.objects[i].backupArrPosition;
             states.objects[i].center = [80, 60, 160/3];
             states.objects[i].rotate = [90, 0, 0];
